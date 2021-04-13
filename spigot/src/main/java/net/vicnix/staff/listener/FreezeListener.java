bf97171e70cb40111e70b58a857f023346dfe125
Xavier <xavier04.martinez@gmail.com>
2021-04-13 15:57:57 -0500

    fix FreezeListener

--- spigot/src/main/java/net/vicnix/staff/listener/FreezeListener.java
+++ spigot/src/main/java/net/vicnix/staff/listener/FreezeListener.java
@@ -2,6 +2,8 @@ package net.vicnix.staff.listener;
 
 import net.vicnix.staff.session.Session;
 import net.vicnix.staff.session.SessionManager;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,31 +17,70 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class FreezeListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onHit(EntityDamageEvent e){
-        if ( e.getEntity() == null || !(e.getEntity() instanceof Player)) return;
-        Session playerSession = SessionManager.getInstance().getSession(e.getEntity().getUniqueId());
-        if(playerSession.isFreezed()) e.setCancelled(true);
+    public void onHit(EntityDamageEvent ev) {
+        Entity entity = ev.getEntity();
+
+        if (!(entity instanceof Player)) return;
+
+        Session session = SessionManager.getInstance().getSession(entity.getUniqueId());
+
+        if (session == null) return;
+
+        if (!session.isFreezed()) return;
+
+        ev.setCancelled(true);
     }
+
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onPlayerAttack(EntityDamageByEntityEvent e){
-        if(e.getDamager() == null || !(e.getDamager() instanceof Player)) return;
-        Session playerSession = SessionManager.getInstance().getSession(e.getDamager().getUniqueId());
-        if(playerSession.isFreezed()) e.setCancelled(true);
+    public void onPlayerAttack(EntityDamageByEntityEvent ev) {
+        Entity entity = ev.getEntity();
+
+        if (!(entity instanceof Player)) return;
+
+        Session session = SessionManager.getInstance().getSession(entity.getUniqueId());
+
+        if (session == null) return;
+
+        if (!session.isFreezed()) return;
+
+        ev.setCancelled(true);
     }
+
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onMove(PlayerMoveEvent e) {
-        if (e.getTo().getBlockX() == e.getFrom().getBlockX() && e.getTo().getBlockY() == e.getFrom().getBlockY() && e.getTo().getBlockZ() == e.getFrom().getBlockZ())  return;
-        Session playerSession = SessionManager.getInstance().getSession(e.getPlayer().getUniqueId());
-        if(playerSession.isFreezed()) e.setTo(e.getFrom());
+    public void onMove(PlayerMoveEvent ev) {
+        Location to = ev.getTo();
+        Location from = ev.getFrom();
+
+        if (to.getBlockX() == from.getBlockX() && to.getBlockY() == from.getBlockY() && to.getBlockZ() == from.getBlockZ()) return;
+
+        Session session = SessionManager.getInstance().getSession(ev.getPlayer().getUniqueId());
+
+        if (session == null) return;
+
+        if (!session.isFreezed()) return;
+
+        ev.setTo(ev.getFrom());
     }
+
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onBlockPlace(BlockPlaceEvent e) {
-        Session playerSession = SessionManager.getInstance().getSession(e.getPlayer().getUniqueId());
-        if(playerSession.isFreezed()) e.setCancelled(true);
+    public void onBlockPlace(BlockPlaceEvent ev) {
+        Session session = SessionManager.getInstance().getSession(ev.getPlayer().getUniqueId());
+
+        if (session == null) return;
+
+        if (!session.isFreezed()) return;
+
+        ev.setCancelled(true);
     }
+
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onBlockBreak(BlockBreakEvent e) {
-        Session playerSession = SessionManager.getInstance().getSession((e.getPlayer().getUniqueId()));
-        if(playerSession.isFreezed()) e.setCancelled(true);
+    public void onBlockBreak(BlockBreakEvent ev) {
+        Session session = SessionManager.getInstance().getSession(ev.getPlayer().getUniqueId());
+
+        if (session == null) return;
+
+        if (!session.isFreezed()) return;
+
+        ev.setCancelled(true);
     }
 }
