38f828401c8a200db7350e193692653ef722b2b3
ale444113 <alemolinahernandez06@gmail.com>
2021-04-16 22:32:26 +0200

    Actions para el Xavier que no sabe (Encima es peruano)

--- spigot/src/main/java/net/vicnix/staff/utils/action/FreezeAction.java
+++ spigot/src/main/java/net/vicnix/staff/utils/action/FreezeAction.java
@@ -9,5 +9,15 @@ public class FreezeAction implements IAction {
         Session session = targets[0];
 
         Session target = targets[1];
+
+        target.setFreezed(!target.isFreezed());
+
+        if (!target.isFreezed()) {
+            target.sendMessage("&8------------------------------");
+            target.sendMessage("&a Fuiste descongelado por " + target.whoFreezed());
+            target.sendMessage("&8------------------------------");
+        }
+
+        target.setWhoFreezed(target.isFreezed() ? session.getSessionStorage().getName() : null);
     }
 }
--- spigot/src/main/java/net/vicnix/staff/utils/action/VanishAction.java
+++ spigot/src/main/java/net/vicnix/staff/utils/action/VanishAction.java
@@ -1,11 +1,23 @@
 package net.vicnix.staff.utils.action;
 
 import net.vicnix.staff.session.Session;
+import net.vicnix.staff.session.SessionStorage;
 
 public class VanishAction implements IAction {
 
     @Override
     public void run(Session... targets) {
         Session target = targets[0];
+        SessionStorage sessionStorage = target.getSessionStorage();
+
+        if (!sessionStorage.isStaff()) {
+            sessionStorage.setStaff(true);
+        }
+
+        sessionStorage.setVanished(!sessionStorage.isVanished());
+
+        target.setDefaultAttributes();
+
+        target.sendMessage("&2Ahora eres &b" + (sessionStorage.isVanished() ? "invisible" : "visible") + "&2 para los demas jugadores.");
     }
 }
